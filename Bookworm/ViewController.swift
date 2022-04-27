540d482a938ead9e1a41db9fe7d6eea7f7cc26c5
Jessicaggan <jessica.ganadhi@binus.ac.id>
2022-04-27 18:51:36 +0700

    Initial Commit

---
+++ Bookworm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bookworm
+//
+//  Created by Jessica Geofanie on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
